81f8071ff1ba9fa03c1adf1f9dd0d082bd193355
CORP\javiguer <guerrerogallojavier@gmail.com>
2022-06-07 12:08:15 +0200

    zx

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -2,18 +2,29 @@ const userController = {}
 
 const User = require('../models/User')
 
+const CryptoJS = require('crypto-js')
+
+const clave = 'phamsfnUHYhbasf738';
+
 userController.getUsers = async (req, res) => {
-    const users = await User.find();
+    let users = await User.find();
+    for(let user of users){
+        user.password = CryptoJS.AES.encrypt(user.password.trim(), clave.trim()).toString();
+    }
     res.json(users);
 }
 
 userController.getUser = async (req, res) => {
-    const user = await User.findOne({email: req.params.email});
+    let user = await User.findOne({email: req.params.email});
+    user.password = CryptoJS.AES.encrypt(user.password.trim(), clave.trim()).toString();
     res.send(user);
 }
 
 userController.editUser = async (req, res) => {
-    await User.findOneAndUpdate({email: req.params.email}, req.body);
+    let editedUser = { email, password, nombre, apellidos, cp, telefono, pc_favoritos, componente_favoritos } = req.body;
+    editedUser.password = CryptoJS.AES.encrypt(editedUser.password.trim(), clave.trim()).toString();
+
+    await User.findOneAndUpdate({email: req.params.email}, editedUser);
     res.send({status: 'usuario actualizado'})
 }
 
