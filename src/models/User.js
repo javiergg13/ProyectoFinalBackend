de0cd8aef4b02d4bc04c309f02eb53b3a06366bd
CORP\javiguer <guerrerogallojavier@gmail.com>
2022-06-07 17:11:28 +0200

    f

--- src/models/User.js
+++ src/models/User.js
@@ -9,7 +9,9 @@ const userSchema = new Schema({
     password: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     cp: {type: Number, required: true},
-    telefono: {type: Number, required: true}
+    telefono: {type: Number, required: true},
+    pc_favoritos:{type: Array, required: true},
+    componente_favoritos:{type: Array, required: true}
 }, {
     timestamps: true,
     versionKey: false
